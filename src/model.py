3a8434c26669df247b5f34db5ff3be19b68d57ea
CPTMinh <156873086+CPTMinh@users.noreply.github.com>
2026-03-21 20:11:21 +0700

    Revert "fix"
    This reverts commit 560c941b4c64dc0babd24656040f2699413d8fa3.

--- src/model.py
+++ src/model.py
@@ -155,7 +155,7 @@ NUMERIC_FEATURES = [
     "is_political_context",
 ]
 
-CATEGORICAL_FEATURES = ["party"]
+CATEGORICAL_FEATURES = ["party", "speaker_job", "state_info"]
 
 
 class LIARDataset(Dataset):
